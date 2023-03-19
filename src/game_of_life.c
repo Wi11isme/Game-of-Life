a2c278d2e21bbf0effac2f2003c7845611b7ad62
Viscount Brown <viscount@si-d2.kzn.21-school.ru>
2023-03-19 17:22:34 +0300

    mod2

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,49 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#define MMAX 25
-#define NMAX 80
+#define HEIGHT 25
+#define WIDTH 80
 
-void input(int field1[][NMAX], int field2[][NMAX]);
-void draw(int field[][NMAX]);
-int new_cell(int field1[][NMAX], int x, int y);
+void input(int** field1, int** field2);
+void draw(int** field);
+int new_cell(int** field, int y, int x);
+void copy(int** field1, int** field2);
 
 int main() {
-    int field1[MMAX][NMAX];
-    for (int i = 0; i < MMAX; i++) {
-        for (int j = 0; j < NMAX; j++) {
-            field1[i][j] = rand() % 2;
+    int** field1 = malloc(HEIGHT * sizeof(int*));
+    int* ptr1 = malloc(HEIGHT * WIDTH * sizeof(int));
+    for (int i = 0; i < HEIGHT; i++) {
+        field1[i] = ptr1 + WIDTH * i;
+    }
+    for (int q = 0; q < HEIGHT; q++) {
+        for (int s = 0; s < WIDTH; s++) {
+            scanf("%d", &field1[q][s]);
         }
     }
-    system("clear");
-    draw(field1);
-    int field2[MMAX][NMAX];
-    input(field1, field2);
-    printf("\n хто я\n");
-
-    // system("clear");
-    draw(field2);
+    for (int i = 0; i < 30; i++) {
+        usleep(300000);
+        system("clear");
+        draw(field1);
+        int** field2 = malloc(HEIGHT * sizeof(int*));
+        int* ptr2 = malloc(HEIGHT * WIDTH * sizeof(int));
+        for (int k = 0; k < HEIGHT; k++) field2[k] = ptr2 + WIDTH * k;
+        input(field1, field2);
+        copy(field1, field2);
+        free(ptr2);
+        free(field2);
+    }
+    free(ptr1);
+    free(field1);
     return 0;
 }
 
-void input(int field1[][NMAX], int field2[][NMAX]) {
-    for (int i = 0; i < MMAX; i++) {
-        for (int j = 0; j < NMAX; j++) {
+void input(int** field1, int** field2) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
             field2[i][j] = new_cell(field1, i, j);
         }
     }
 }
 
-void draw(int field1[][NMAX]) {
-    for (int i = 0; i < MMAX; i++) {
-        for (int j = 0; j < NMAX; j++) {
-            if (field1[i][j]) {
-                if (j == NMAX - 1)
+void draw(int** field) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (field[i][j]) {
+                if (j == WIDTH - 1)
                     printf("#\n");
                 else
                     printf("#");
             } else {
-                if (j == NMAX - 1)
+                if (j == WIDTH - 1)
                     printf(".\n");
                 else
                     printf(".");
@@ -52,18 +63,27 @@ void draw(int field1[][NMAX]) {
     }
 }
 
-int new_cell(int field1[][NMAX], int x, int y) {
-    int result_cell = field1[x][y], count = 0;
-    for (int i = x - 1; i < x + 2; i++) {
-        for (int j = y - 1; j < y + 2; j++) {
-            if (i != x || j != y) {
-                if (field1[i][j]) count++;
-            }
-        }
-    }
-    if (field1[x][y]) {
+int new_cell(int** field, int y, int x) {
+    int result_cell = field[y][x], count;
+    x += WIDTH;
+    y += HEIGHT;
+    count = field[(y - 1) % HEIGHT][(x - 1) % WIDTH] + field[(y - 1) % HEIGHT][x % WIDTH] +
+            field[(y - 1) % HEIGHT][(x + 1) % WIDTH] + field[y % HEIGHT][(x - 1) % WIDTH] +
+            field[y % HEIGHT][(x + 1) % WIDTH] + field[(y + 1) % HEIGHT][(x - 1) % WIDTH] +
+            field[(y + 1) % HEIGHT][x % WIDTH] + field[(y + 1) % HEIGHT][(x + 1) % WIDTH];
+    x -= WIDTH;
+    y -= HEIGHT;
+    if (field[y][x]) {
         if (count != 2 && count != 3) result_cell = 0;
     } else if (count == 3)
         result_cell = 1;
     return result_cell;
+}
+
+void copy(int** copy_field, int** field_orig) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            copy_field[i][j] = field_orig[i][j];
+        }
+    }
 }
